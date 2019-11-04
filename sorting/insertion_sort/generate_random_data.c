f170a33fc58c72c19e8037f9ef78fb238ac7294d
Malleswara Reddy M <malleswarareddy.coding@gmail.com>
2019-11-04 23:59:40 +0530

    insertion sort has been added

---
+++ sorting/insertion_sort/generate_random_data.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(int argc, char **argv)
+{
+	FILE *fp = fopen("test_data","w");
+
+	if(fp==NULL)
+	{
+		printf("unable to generate random data... File Error\n");
+		return 1;
+	}
+
+	long int num_case = atol(argv[1]);
+
+	fprintf(fp,"%ld\n",num_case);
+
+	for(long int i=0; i<num_case; ++i)
+	{
+		long int num_values = atol(argv[2+i]);
+		fprintf(fp,"%ld\n",num_values);
+		srand(time(NULL));
+		for(long int j=0;j<num_values;++j)
+		{
+			fprintf(fp,"%ld ", random());
+		}
+		fprintf(fp, "\n");
+	}
+
+	fclose(fp);
+	return 0;
+}
---
+++ sorting/insertion_sort/insertion_sort.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+void insertion_sort(long *list, long size)
+{
+	for(long i=1; i<size; ++i)
+	{
+		long key = list[i];
+		long j = i-1;
+		while(j>=0 && key < list[j])
+		{
+			list[j+1] = list[j];
+			--j;
+		}
+		list[j+1] = key;
+	}
+}
+
+void print_list(long *list, long size)
+{
+	for(long i=0; i<size; ++i)
+	{
+		printf("%ld ",list[i]);
+	}
+	printf("\n");	
+}
+
+int main()
+{
+	long num_cases,*list;
+	scanf("%ld",&num_cases);
+	for(long i=0;i<num_cases;++i)
+	{
+		long num_values;
+		scanf("%ld ",&num_values);
+		list = (long *)malloc(sizeof(long)*num_values);
+		for(int j=0;j<num_values;++j)
+		{
+			scanf("%ld ", list+j);
+		}
+		clock_t t = clock();
+		insertion_sort(list, num_values);
+		t = clock() - t;
+		double time_taken = (double)t/CLOCKS_PER_SEC;
+		printf("time_taken to sort %ld elements is %lf\n",num_values,time_taken);
+		print_list(list,num_values);
+		free(list);
+	}
+
+	return 0;
+}
