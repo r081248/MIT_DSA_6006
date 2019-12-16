0d5b8acd5eb62dd9b293f8ea05a913c5925b63f2
Malleswara Reddy Mula <malleswarareddy.coding@gmail.com>
2019-12-16 18:34:08 +0530

    destructor has been added

--- data_structures/linked_list/list.cpp
+++ data_structures/linked_list/list.cpp
@@ -2,36 +2,24 @@
 #include <iostream>
 
 template<typename T>
-<<<<<<< HEAD
 bool list<T>::add(T value)
-=======
-bool list<T>::insert(T value) //Add at beginning
->>>>>>> 6031b641e4dc36b5ffc5e95e98564d093af32208
 {
     m_data = new node(value, m_data);
     return m_data;
 }
 
 template<typename T>
-<<<<<<< HEAD
 bool list<T>::remove(T value)
 {
     
     node *temp = m_data;
     node *prev;
-=======
-bool list<T>::deletion(T value)
-{
-    node<T> *temp = m_data;
-    node<T> *prev;
->>>>>>> 6031b641e4dc36b5ffc5e95e98564d093af32208
     while(temp)
     {
         if(temp->value == value)
         {
             if(temp == m_data)
             {
-<<<<<<< HEAD
                 m_data = m_data->next;
             }   
             else
@@ -41,22 +29,26 @@ bool list<T>::deletion(T value)
             delete temp;
             return true;
             
-=======
-                m_data = temp->next; // If the element deleted to be the first one
-            }
-            else
-            {
-                prev->next = temp->next;       
-            }
-            delete temp;
-            return true;//if key is deleted successfully
->>>>>>> 6031b641e4dc36b5ffc5e95e98564d093af32208
         }
         prev = temp;
         temp = temp->next;
     }
-<<<<<<< HEAD
     return false;
+}
+
+template<typename T>
+list<T>::~list()
+{
+    node *temp;
+
+    while(m_data)
+    {
+        temp = m_data;
+        m_data = m_data->next;
+        delete temp;
+    }
+
+    return;
 }
 
 /*template<typename T>
@@ -132,78 +124,5 @@ int main()
         }     
     }
     return 0;
-=======
-    return false; //If key is not found in the list
-}
-
-template<typename T>
-void list<T>::printList()
-{
-    node<T> *iter = m_data;
-    while(iter) 
-    {
-        std::cout << iter->value << " ";
-        iter = iter->next; 
-    }
-    std::cout << std::endl;  
-}
-
-template<typename T>
-list<T>::~list()
-{
-    if(m_data)
-    {
-        delete m_data;
-    }
-}
-
-
-#if 0
-int main()
-{
-    list<int> ll;
-    bool alive = true;
-    char decision;
-    int data;
-    while(alive)
-    {
-        std::cout << "insert data : (i)\n";
-        std::cout << "delete data : (d)\n";
-        std::cout << "show the list:(s)\n";
-        std::cout << "exit the application : (e)\n";
-        std::cin >> decision;
-        switch(decision)
-        {
-            case 'i':
-                std::cout << "Enter the data to insert: ";
-                std::cin >> data;
-                ll.insert(data);           
-                break;
-            case 'd':
-                std::cout << "Enter the data that to be deleted from list: ";
-                std::cin >> data;
-                if(ll.deletion(data))
-                {
-                    std::cout << data << " has been deleted from the list\n";
-                }
-                else
-                {
-                    std::cout << data << " is not found in the list\n";
-                }
-                
-                break;
-            case 's':
-                ll.printList();
-                break;
-            case 'e':
-                alive = false;
-                break;
-            default:
-                std::cout << "invalid option selected... Try again\n";
-                break;
-        }
-        
-    }
->>>>>>> 6031b641e4dc36b5ffc5e95e98564d093af32208
 }
 #endif
