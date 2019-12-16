e670d36743a6795d5e95026ffd1c8f557279f590
Malleswara Reddy Mula <malleswarareddy.coding@gmail.com>
2019-12-16 17:29:43 +0530

    iterator model of list has been implemented

--- data_structures/linked_list/list.cpp
+++ data_structures/linked_list/list.cpp
@@ -1,14 +1,112 @@
 #include "list.h"
 
 template<typename T>
-bool list<T>::insert(T value)
+bool list<T>::add(T value)
 {
-    m_data = new node<T>(value,m_data);
+    m_data = new node(value, m_data);
     return m_data;
 }
 
 template<typename T>
-bool list<T>::delete(T value)
+bool list<T>::remove(T value)
 {
-;
+    
+    node *temp = m_data;
+    node *prev;
+    while(temp)
+    {
+        if(temp->value == value)
+        {
+            if(temp == m_data)
+            {
+                m_data = m_data->next;
+            }   
+            else
+            {
+                prev->next = temp->next;
+            }
+            delete temp;
+            return true;
+            
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return false;
 }
+
+/*template<typename T>
+list<T>::iterator list<T>::begin() noexcept
+{
+    return iterator(m_data);
+}
+
+template<typename T>
+list<T>::iterator list<T>::end() noexcept
+{
+    return iterator(nullptr);
+}
+*/
+#if 0
+#include<iostream>
+#include<vector>
+int main()
+{
+
+    list<int> lst;
+    bool alive = true;
+    char option;
+    int value;
+    while(alive)
+    {
+        std::cout << "add node : [a]\n";
+        std::cout << "remove node: [r]\n";
+        std::cout << "print list: [p]\n";
+        std::cout << "exit: [e]\n";
+        std::cin >> option;
+        switch (option)
+        {
+        case 'a':
+            std::cout << "Enter value: ";
+            std::cin >> value;
+            lst.add(value);
+            break;
+        case 'r':
+            std::cout << "Enter the value to be deleted: ";
+            std::cin >> value;
+            if(lst.remove(value))
+            {
+                std::cout << value << " has been deleted from the list" << std::endl;
+            }
+            else
+            {
+                std::cout << value << " is not found from the list" << std::endl;
+            }
+            break;
+        case 'p':
+            /*for(list<int>::iterator it = lst.begin(); it != lst.end();it++)
+            {
+                std::cout << *it << " ";
+            }
+            std::cout << std::endl;
+            */
+
+            for(auto it:lst)
+            {
+                std::cout << it << " ";
+                it = -50;
+            }
+            std::cout << std::endl;
+            break;
+        case 'e':
+            alive = false;
+            break;
+        
+        default:
+            std::cout << "Invalid option... retry\n";
+            break;
+        }     
+    }
+    return 0;
+}
+#endif
--- data_structures/linked_list/list.h
+++ data_structures/linked_list/list.h
@@ -1,26 +1,72 @@
-template<typename T>
-class node
-{
-public:
-    node(T data, node *ptr):
-    value{data},
-    next{ptr}
-    {
-    }
-    T value;
-    node *next;
-};
 
 template<typename T>
 class list
 {
+
 public:
-    list(){};
-    bool insert(T value);
-    bool delete(T value);
+
+//Farward Declarations
+    class node;
+    class iterator;
+//==========================
+
+    list():m_data{nullptr}{};
+    bool add(T value);
+    bool remove(T value);
+    iterator begin() 
+    {
+        return iterator(m_data);
+    }
+    iterator end() 
+    {
+        return iterator(nullptr);
+    }
+
+    class iterator
+    {
+        public:
+            iterator():currentNode{m_data}{};
+            iterator(node* startingNode):
+            currentNode{startingNode}
+            {
+            };
+            T operator*(){return currentNode->value;};
+            iterator& operator++()
+            {
+                if(currentNode)
+                {
+                    currentNode = currentNode->next;
+                }
+                return *this;   
+            }
+            iterator operator++(int)
+            {
+                iterator it = *this;
+                ++*this;
+                return it;
+            }
+            bool operator!=(const iterator &it)
+            {
+                return !(this->currentNode==it.currentNode);
+            }
+
+        private:
+            node *currentNode;
+    };
 
 private:
-    node<T> *createNode(T value, node<T>*next);
-    node<T> *m_data;
+    node* m_data;
+
+    class node // Making not available to outside
+    {
+    friend class list;
+    
+    public:
+        node(T data, node *ptr):value{data},next{ptr}{}
+    
+    private:
+        T value;
+        node *next;
+    };
 
 };
